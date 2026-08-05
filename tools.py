9e9d755518e810bc627ef8f898d978cd4f3b1f26
Mohan C P <cpm4kor@bosch.com>
2026-08-05 15:06:20 +0530

    Update tools.py v2

--- tools.py
+++ tools.py
@@ -71,7 +71,7 @@ def get_current_date() -> str:
 _ALLOWED_OPERATORS = {
     ast.Add: operator.add,
     ast.Sub: operator.sub,
-    ast.Multi: operator.mul,
+    ast.Mult: operator.mul,
     ast.Div: operator.truediv,
     ast.FloorDiv: operator.floordiv,
     ast.Mod: operator.mod,
